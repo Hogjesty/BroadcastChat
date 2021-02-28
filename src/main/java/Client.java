a95d80671f952b9fcd74f7c48ad05daf7d0be8a1
Hogjesty <64978483+Hogjesty@users.noreply.github.com>
2021-02-28 18:31:47 +0200

    Alpha-client is created

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,6 +1,20 @@
 public class Client {
 
     public static void main(String[] args) {
-
+        int port = Integer.parseInt(args[0]);
+        name = args[1];
+        try {
+            InetAddress ipAddress = InetAddress.getByName(address);
+            System.out.println("Hello, " + name + "!\nYou are connecting to " + address + ":" + port);
+            Socket socket = new Socket(ipAddress, port);
+            System.out.println("Connection established!");
+            Thread input = new Thread(new Input(new DataInputStream(socket.getInputStream())));
+            Thread output = new Thread(new Output(new DataOutputStream(socket.getOutputStream())));
+            input.start();
+            output.start();
+        } catch (IOException e) {
+            System.err.println("Unable to setup connection...");
+            e.printStackTrace();
+        }
     }
 }
